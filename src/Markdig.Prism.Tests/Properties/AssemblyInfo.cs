452a6c27b08437f95d6349df1384512a2ca7e04c
Ilya Verbitskiy <ilya@verbitskiy.co>
2020-10-21 23:41:11 +0800

    initial commit

---
+++ src/Markdig.Prism.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Markdig.Prism.Tests")]
+[assembly: AssemblyDescription("Ilya Verbitskiy")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("WebStoating s.r.o.")]
+[assembly: AssemblyProduct("Markdig.Prism")]
+[assembly: AssemblyCopyright("Copyright © 2020 Ilya Verbitskiy")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ac5dedfa-98a6-435a-ba99-bea016429c41")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
