6b05576e0cb4c89c28677bbd12df8a0242a42dda
Tosic.killer <emil.schutt@gmail.com>
2023-07-05 14:28:32 +0200

    Added an option to use line-numbers plugin for PrismJS, also added a way to expand the number of supported plugins using an options class

--- src/Markdig.Prism/PrismCodeBlockRenderer.cs
+++ src/Markdig.Prism/PrismCodeBlockRenderer.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Versioning;
+﻿using System;
+using System.Runtime.Versioning;
 using System.Text;
 using System.Web;
 using Markdig.Parsers;
@@ -8,72 +9,82 @@ using Markdig.Syntax;
 
 namespace Markdig.Prism
 {
-    public class PrismCodeBlockRenderer : HtmlObjectRenderer<CodeBlock>
+  public class PrismCodeBlockRenderer : HtmlObjectRenderer<CodeBlock>
+  {
+    private readonly CodeBlockRenderer codeBlockRenderer;
+    // Adding options to the renderer
+    private readonly PrismOptions _options;
+
+    public PrismCodeBlockRenderer(CodeBlockRenderer codeBlockRenderer, PrismOptions options)
     {
-        private readonly CodeBlockRenderer codeBlockRenderer;
-
-        public PrismCodeBlockRenderer(CodeBlockRenderer codeBlockRenderer)
-        {
-            this.codeBlockRenderer = codeBlockRenderer ?? new CodeBlockRenderer();
-        }
-
-        protected override void Write(HtmlRenderer renderer, CodeBlock node)
-        {
-            var fencedCodeBlock = node as FencedCodeBlock;
-            var parser = node.Parser as FencedCodeBlockParser;
-            if (fencedCodeBlock == null || parser == null)
-            {
-                codeBlockRenderer.Write(renderer, node);
-                return;
-            }
-
-            var languageCode = fencedCodeBlock.Info.Replace(parser.InfoPrefix, string.Empty);
-            if (string.IsNullOrWhiteSpace(languageCode) || !PrismSupportedLanguages.IsSupportedLanguage(languageCode))
-            {
-                codeBlockRenderer.Write(renderer, node);
-                return;
-            }
-
-            var attributes = new HtmlAttributes();
-            attributes.AddClass($"language-{languageCode}");
-
-            var code = ExtractSourceCode(node);
-            var escapedCode = HttpUtility.HtmlEncode(code);
-
-            renderer
-                .Write("<pre>")
-                .Write("<code")
-                .WriteAttributes(attributes)
-                .Write(">")
-                .Write(escapedCode)
-                .Write("</code>")
-                .Write("</pre>");
-        }
-
-        protected string ExtractSourceCode(LeafBlock node)
-        {
-            var code = new StringBuilder();
-            var lines = node.Lines.Lines;
-            int totalLines = lines.Length;
-            for (int i = 0; i < totalLines; i++)
-            {
-                var line = lines[i];
-                var slice = line.Slice;
-                if (slice.Text == null)
-                {
-                    continue;
-                }
-
-                var lineText = slice.Text.Substring(slice.Start, slice.Length);
-                if (i > 0)
-                {
-                    code.AppendLine();
-                }
-
-                code.Append(lineText);
-            }
-
-            return code.ToString();
-        }
+      this.codeBlockRenderer = codeBlockRenderer ?? new CodeBlockRenderer();
+      // If no options are given we use the defaults that are staed in the PrismOptions class
+      _options = options ?? new PrismOptions();
     }
+
+    protected override void Write(HtmlRenderer renderer, CodeBlock node)
+    {
+      var fencedCodeBlock = node as FencedCodeBlock;
+      var parser = node.Parser as FencedCodeBlockParser;
+      if (fencedCodeBlock == null || parser == null)
+      {
+        codeBlockRenderer.Write(renderer, node);
+        return;
+      }
+
+      var languageCode = fencedCodeBlock.Info.Replace(parser.InfoPrefix, string.Empty);
+      if (string.IsNullOrWhiteSpace(languageCode) || !PrismSupportedLanguages.IsSupportedLanguage(languageCode))
+      {
+        codeBlockRenderer.Write(renderer, node);
+        return;
+      }
+
+      var attributes = new HtmlAttributes();
+      attributes.AddClass($"language-{languageCode}");
+
+      // for this plugin its a simple class that needs to be added but others might need to have óther things to work
+      if (_options.UseLineNumbers)
+      {
+        attributes.AddClass("line-numbers");
+      }
+
+      var code = ExtractSourceCode(node);
+      var escapedCode = HttpUtility.HtmlEncode(code);
+
+      renderer
+          .Write("<pre>")
+          .Write("<code")
+          .WriteAttributes(attributes)
+          .Write(">")
+          .Write(escapedCode)
+          .Write("</code>")
+          .Write("</pre>");
+    }
+
+    protected string ExtractSourceCode(LeafBlock node)
+    {
+      var code = new StringBuilder();
+      var lines = node.Lines.Lines;
+      int totalLines = lines.Length;
+      for (int i = 0; i < totalLines; i++)
+      {
+        var line = lines[i];
+        var slice = line.Slice;
+        if (slice.Text == null)
+        {
+          continue;
+        }
+
+        var lineText = slice.Text.Substring(slice.Start, slice.Length);
+        if (i > 0)
+        {
+          code.AppendLine();
+        }
+
+        code.Append(lineText);
+      }
+
+      return code.ToString();
+    }
+  }
 }
---
+++ src/Markdig.Prism/PrismOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Markdig.Prism
+{
+  public class PrismOptions
+  {
+    public bool UseLineNumbers { get; set; } = false;
+  }
+}
